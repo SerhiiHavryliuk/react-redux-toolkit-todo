c02f275a380b9ed7f6546c00a1d212621d7cbcf2
Serhii H Inv <serhii.havryliuk@eliftech.com>
2023-07-02 17:50:34 +0300

    add todo app v2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [textInput, setTextInput] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const setInputText = (event) => {
+    setTextInput(event.target.value);
+  }
+
+  const addNewTodo = () => {
+    const newTodoList = [
+      ...todoList,
+      {
+        id: new Date().toString(),
+        text: textInput,
+        isComplete: false,
+      }
+    ]
+    setTodoList(newTodoList);
+    setTextInput('')
+  }
+
+  const deleteTodo = (idTodo) => {
+    const newTodoList = todoList.filter(
+      (todo)=> todo.id !== idTodo
+    )
+    setTodoList(newTodoList);
+  }
+
+  const toggleTodoComplete = (idTodo) =>{
+    const newTodoList = todoList.map(
+      (todo)=>{
+        if(todo.id !== idTodo) return todo;
+
+          return{
+          ...todo,
+            isComplete: !todo.isComplete
+          }
+      }
+    )
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='todoWrap'>
+        <div className='inputTextWrap'>
+          <input type='text' className='inputText' value={textInput} onChange={setInputText}/>
+          <button className='btnAddNewTodo' onClick={addNewTodo}>Add</button>
+
+          <ol>
+            {
+              todoList.map((todo) => (
+                <li key={todo.id}>
+                  <input type='checkbox' onChange={()=>toggleTodoComplete(todo.id)}/>
+                  <span className={`todoItem ${todo.isComplete?'complete': ''}`}>{todo.text}</span>
+                  <input type='button' value='delete' onClick={() => deleteTodo(todo.id)}/>
+                </li>
+              ))
+            }
+          </ol>
+        </div>
+      </div>
     </div>
   );
 }
